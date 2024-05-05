278e00cf3aee862925295049f9991b02de21d023
Carlos Reyes Stoneham <stoneham@live.com>
2024-05-05 16:32:34 -0400

    double quotes and catch error

--- src/api/eigenlayer.ts
+++ src/api/eigenlayer.ts
@@ -7,12 +7,17 @@ type EigenLayerResponse = any;
 
 router.get<{}, EigenLayerResponse>("/", async (req, res) => {
   // fetch from eigenlayer
-  const address = req.query.address as string;
-  const response = await axios.get(
-    `https://claims.eigenfoundation.org/clique-eigenlayer-api/campaign/eigenlayer/credentials?walletAddress=${address}`
-  );
+  try {
+    const address = req.query.address as string;
+    const response = await axios.get(
+      `https://claims.eigenfoundation.org/clique-eigenlayer-api/campaign/eigenlayer/credentials?walletAddress=${address}`
+    );
 
-  res.json(response.data);
+    res.json(response.data);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: "Error fetching data from eigenlayer" });
+  }
 });
 
 export default router;
